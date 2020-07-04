450721c263cf626618ed7ad2f8bd98a3dc4a4ef4
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-07-04 15:03:50 +0700

    get to post

--- src/controller/SandwichController.java
+++ src/controller/SandwichController.java
@@ -1,13 +1,13 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SandwichController {
-    @GetMapping("/saveChoice")
+    @PostMapping("/saveChoice")
     public ModelAndView save(@RequestParam("condiment") String[] condiments) {
         StringBuilder saveCondiments = new StringBuilder();
 
--- web/WEB-INF/views/index.jsp
+++ web/WEB-INF/views/index.jsp
@@ -5,7 +5,7 @@
 </head>
 <body>
 <h1>Sandwich Condiments</h1>
-<form action="saveChoice">
+<form action="saveChoice" method="post">
     <input type="checkbox" name="condiment" value="Lettuce">Lettuce
     <input type="checkbox" name="condiment" value="Tomato">Tomato
     <input type="checkbox" name="condiment" value="Mustard">Mustard
